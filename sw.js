c53cca93d283d26ed609d84a99c42367b2dcda0c
Xikumi <1374415225@qq.com>
2022-03-13 21:09:57 +0800

    Build at 2022-03-13 21:09:57 +08:00

--- sw.js
+++ sw.js
@@ -10,7 +10,7 @@
 
 'use strict';
 
-var precacheConfig = [];
+var precacheConfig = [["/sw-register.js","b61e8f1304abd174d27e366aeeb2a99b"]];
 var cacheName = 'sw-precache-v3--' + (self.registration ? self.registration.scope : '');
 var firstRegister = 1; // 默认1是首次安装SW， 0是SW更新
 
